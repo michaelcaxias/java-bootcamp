8f774fdc1e7f37bccc5146929c7ffa293fd34440
Michael Caxias <michael.caxias@mercadolivre.com>
2022-09-22 15:13:01 -0300

    feat: exemplo com if e else

--- src/exemplo01/Exemplo02.java
+++
@@ -1,4 +0,0 @@
-package exemplo01;
-
-public class Exemplo02 {
-}
---
+++ src/exemplo02/Exemplo02.java
@@ -0,0 +1,12 @@
+package exemplo02;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        int number = 5;
+        if (number > 0) {
+            System.out.println("O numero é maior que 0");
+        } else {
+            System.out.println("O valor é menor ou igual a 0");
+        }
+    }
+}
