d04b7d034a7db8fe88816a8fb7fcb3f87b098698
Michael Caxias <michael.caxias@mercadolivre.com>
2022-09-22 20:44:43 -0300

    style: identa linhas corretamente

--- src/poo_java_1/exercises/MaxMinTemperature.java
+++ src/poo_java_1/exercises/MaxMinTemperature.java
@@ -2,8 +2,31 @@ package poo_java_1.exercises;
 
 public class MaxMinTemperature {
     public static void main(String[] args) {
-        String[] cities = {"Londres", "Madrid", "New York", "Buenos Aires", "Asuncion", "Sao Paulo", "Lima", "Santiago", "Lisboa", "Tokio" };
-        int[][] temperatures = {{-2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42}, {5, 43}, {0, 39}, {-7, 26}, {-1, 31}, {-10, 35}};
+        String[] cities = {
+                "Londres",
+                "Madrid",
+                "New York",
+                "Buenos Aires",
+                "Asuncion",
+                "Sao Paulo",
+                "Lima",
+                "Santiago",
+                "Lisboa",
+                "Tokio"
+        };
+
+        int[][] temperatures = {
+                {-2, 33},
+                {-3, 32},
+                {-8, 27},
+                {4, 37},
+                {6, 42},
+                {5, 43},
+                {0, 39},
+                {-7, 26},
+                {-1, 31},
+                {-10, 35}
+        };
 
         int min = temperatures[0][0];
         int max = 0;
@@ -11,12 +34,11 @@ public class MaxMinTemperature {
         String minCity = cities[0];
         String maxCity = cities[0];
 
-        for (int linha = 0; linha < 10; linha++) {
-            String city = cities[linha];
-            System.out.println(city);
+        for (int line = 0; line < 10; line++) {
+            String city = cities[line];
 
             for (int column = 0; column < 2; column++) {
-                int temperature = temperatures[linha][column];
+                int temperature = temperatures[line][column];
 
                 if (temperature < min) {
                     min = temperature;
@@ -27,10 +49,7 @@ public class MaxMinTemperature {
                     max = temperature;
                     maxCity = city;
                 }
-
-                System.out.print(temperatures[linha][column] + " ");
             }
-            System.out.println();
         }
 
         System.out.println(String.format("A temperatura mais baixa foi %s da cidade %s", min, minCity));
