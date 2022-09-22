3b42b888a501761b0429daea57f957112c40bdea
Michael Caxias <michael.caxias@mercadolivre.com>
2022-09-22 15:42:24 -0300

    feat: testa uso do for em exemplo3

---
+++ src/exemplo03/Exemplo03.java
@@ -0,0 +1,10 @@
+package exemplo03;
+
+public class Exemplo03 {
+
+    public static void main(String[] args) {
+        for (int contador = 0; contador < 10; contador++) {
+            System.out.println(contador);
+        }
+    }
+}
