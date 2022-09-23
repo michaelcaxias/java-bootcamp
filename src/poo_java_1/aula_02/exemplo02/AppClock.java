cd22933a148f2791f5d2e521e817583c1eb9501b
Michael Caxias <michael.caxias@mercadolivre.com>
2022-09-23 10:19:09 -0300

    feat: cria metodos e atributos de relogio

--- src/poo_java_1/aula_02/exemplo02/AppClock.java
+++ src/poo_java_1/aula_02/exemplo02/AppClock.java
@@ -4,6 +4,10 @@ public class AppClock {
     public static void main(String[] args) {
         Clock pulseClock = new Clock();
 
+        pulseClock.setHour(24);
+        pulseClock.setMinute(40);
+        pulseClock.setSecond(33);
+
         pulseClock.view();
     }
 }
--- src/poo_java_1/aula_02/exemplo02/Clock.java
+++ src/poo_java_1/aula_02/exemplo02/Clock.java
@@ -4,7 +4,30 @@ public class Clock {
     private int hour, minute, second;
 
     public void setHour(int newHour) {
-        hour = newHour;
+
+        if (newHour >= 24) {
+            hour = 23;
+        } else {
+            hour = Math.max(newHour, 0);
+        }
+    }
+
+    public void setMinute(int newMinute) {
+
+        if (newMinute >= 60) {
+            minute = 59;
+        } else {
+            minute = Math.max(newMinute, 0);
+        }
+    }
+
+    public void setSecond(int newSecond) {
+
+        if (newSecond >= 60) {
+            second = 59;
+        } else {
+            second = Math.max(newSecond, 0);
+        }
     }
 
     public void view() {
