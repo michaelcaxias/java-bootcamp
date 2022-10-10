2e5a166c0d187ad6988f981cc4851484288c3205
Michael Caxias <michael.caxias@mercadolivre.com>
2022-10-10 15:35:02 -0300

    :sparkles: feat: adiciona mensagens para campo dto

--- testing/aula1/src/main/java/com/testing/aula1/dto/UserDto.java
+++ testing/aula1/src/main/java/com/testing/aula1/dto/UserDto.java
@@ -2,18 +2,20 @@ package com.testing.aula1.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Data
 public class UserDto {
 
-    @NotBlank
+    @NotBlank(message = "O nome é obrigatório")
     private String name;
 
-    @NotBlank
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "O email deve estar em um formato válido")
     private String email;
 
-    @Min(value = 18)
+    @Min(value = 18, message = "A idade mínima é 18 anos")
     private int age;
 }
