acf92f8e106402737886bfe7b36ab3a9e9c0d4bd
Michael Caxias <michael.caxias@mercadolivre.com>
2022-10-25 17:22:31 -0300

    feat: cria model TestCase

---
+++ storage/exercises/storage_integrator/src/main/java/com/michael/storage_integrator/model/TestCase.java
@@ -0,0 +1,30 @@
+package com.michael.storage_integrator.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class TestCase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_case;
+
+    @Column(length = 100)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean tested;
+
+    @Column(nullable = false)
+    private boolean passed;
+
+    private int number_of_tries;
+    private LocalDateTime last_update;
+}
--- storage/storage03/src/main/java/com/michael/storage03/controller/TutorialController.java
+++ storage/storage03/src/main/java/com/michael/storage03/controller/TutorialController.java
@@ -2,6 +2,7 @@ package com.michael.storage03.controller;
 
 import com.michael.storage03.model.Tutorial;
 import com.michael.storage03.service.ITutorialService;
+import com.michael.storage03.service.TutorialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
